47fc1c60e773d760832624fdb63f4c04b57ac9af
Robin Wieruch <wrobin@gmx.net>
2017-05-10 01:30:50 +0800

    grid, snake, snack initial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import cs from 'classnames';
+
 import './App.css';
 
+var GRID_ARRAY = [];
+
+for (var i = 0; i <= 50; i++) {
+  GRID_ARRAY.push(i);
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      snake: {
+        position: {
+          x: 10,
+          y: 25,
+        },
+      },
+      snack: {
+        position: {
+          x: 40,
+          y: 25,
+        },
+      },
+    }
+  }
+
   render() {
+    const { snake, snack } = this.state;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Grid
+          snake={snake}
+          snack={snack}
+        />
       </div>
     );
   }
+}
+
+const Grid = ({ snake, snack }) =>
+  <div className="grid">
+    {GRID_ARRAY.map(y => <Row
+      y={y}
+      key={y}
+      snake={snake}
+      snack={snack}
+    />)}
+  </div>
+
+const Row = ({ snake, snack, y }) =>
+  <div className="grid-row">
+    {GRID_ARRAY.map(x => <Cell
+      x={x}
+      y={y}
+      key={x}
+      snake={snake}
+      snack={snack}
+    />)}
+  </div>
+
+const isPosition = (x, y, diffX, diffY) =>
+  x === diffX && y === diffY;
+
+const Cell = ({
+  snake,
+  snack,
+  x,
+  y,
+}) => {
+  const cellCs = cs(
+    "grid-cell",
+    {
+      "grid-cell-snake": isPosition(x, y, snake.position.x, snake.position.y),
+      "grid-cell-snack": isPosition(x, y, snack.position.x, snack.position.y),
+    }
+  );
+
+  return <div className={cellCs} />;
 }
 
 export default App;
